6b543c850ab3be49de1d9089db293ffd5546b28e
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-05-03 16:02:33 +0300

    change the function

--- python/challenges/array_reverse/array_reverse.py
+++ python/challenges/array_reverse/array_reverse.py
@@ -1,10 +1,3 @@
-# my_list =[1,2,3,4,7,6]
-import time
-
-
-# def reverse1(my_list):
-#     reversed_list = my_list[::-1]
-#     return reversed_list
 
 
 def reverse(my_list):
@@ -12,18 +5,9 @@ def reverse(my_list):
         return f"{my_list} is not a list"
     elif len(my_list) ==0:
         return 'list should not be empty'
-    reverse = []
-    for item in my_list:
-        reverse.insert(0, item)
-    return reverse
+    reversed_list = my_list[::-1]
+    return reversed_list
 
-
-# except1 = [1]
-# start = time.time()
-# reverse(except1)
-# end = time.time()
-
-# print(f"Runtime of the function is {end - start}")
 
 if __name__ == "__main__":
     riddle_index = 0
