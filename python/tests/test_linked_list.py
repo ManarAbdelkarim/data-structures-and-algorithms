a4a761b435a96ee464fd03ab0acccbad56f5272d
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-05-06 21:55:44 +0300

    solved

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,4 +1,22 @@
-# from linked_list.linked_list import LinkedList
+import pytest
+from challenges.data_structure.linked_list.linked_list import *
 
-# def test_import():
-#     assert LinkedList
+
+def test_insert(list_test):
+    excpected = "{10} -> {Manar} -> {Reem} ->  NULL"
+    actual = f"{list_test}"
+    assert excpected == actual
+
+def test_includes(list_test):
+    actual = [list_test.includes(55),list_test.includes("Manar"),list_test.includes(5)]
+    excpected = [False, True , False]
+    assert excpected == actual
+
+
+@pytest.fixture
+def list_test():
+    linked = Linked_list()
+    linked.insert("Reem")
+    linked.insert("Manar")
+    linked.insert(10)
+    return linked
