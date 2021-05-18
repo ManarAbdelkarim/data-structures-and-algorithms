e0069800acd4112de8631e293052e3bf67c6c94e
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-05-18 22:37:19 +0300

    solved

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -31,6 +31,31 @@ def test_kthFromEnd(list_test):
     assert actual == expected
     
 
+
+def test_zipList(list_test1,list_test2):
+    actual = Linked_list.zipLists(list_test1,list_test2)
+    excpected = "{1} -> {5} -> {3} -> {9} -> {2} -> {4} ->  NULL"
+    assert excpected == actual
+
+def test_zipList_unbalance1(list_test1,list_test2):
+    list_test1.append(4)
+    actual = Linked_list.zipLists(list_test1,list_test2)
+    excpected = "{1} -> {5} -> {3} -> {9} -> {2} -> {4} -> {4} ->  NULL"
+    assert excpected == actual
+
+def test_zipList_unbalance2(list_test1,list_test2):
+    list_test2.append(4)
+    actual = Linked_list.zipLists(list_test1,list_test2)
+    excpected = "{5} -> {1} -> {9} -> {3} -> {4} -> {2} -> {4} ->  NULL"
+    assert excpected == actual
+
+def test_zipList_None(list_test1):
+    list_test2 = Linked_list()
+    actual = Linked_list.zipLists(list_test1,list_test2)
+    excpected = "{1} -> {3} -> {2} ->  NULL"
+    assert excpected == actual
+
+
 @pytest.fixture
 def list_test():
     linked = Linked_list()
@@ -38,3 +63,19 @@ def list_test():
     linked.insert("Manar")
     linked.insert(10)
     return linked
+
+@pytest.fixture
+def list_test1():
+    linked1 = Linked_list()
+    linked1.append(1)
+    linked1.append(3)
+    linked1.append(2)
+    return linked1
+
+@pytest.fixture
+def list_test2():
+    linked2 = Linked_list()
+    linked2.append("5")
+    linked2.append("9")
+    linked2.append(4)
+    return linked2
