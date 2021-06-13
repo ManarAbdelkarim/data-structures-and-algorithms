ac336d49b1822c269f60d1aba931c2fa838c5d8b
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-06-13 23:30:39 +0300

    gonna fix it

--- python/challenges/data_structure/tree/tree.py
+++ python/challenges/data_structure/tree/tree.py
@@ -1,4 +1,4 @@
-# import pysnooper
+import pysnooper
 # from challenges.data_structure.stacks_and_queues.stacks_and_queues import *
 
 
@@ -187,70 +187,83 @@ class Binary_tree:
      return self.bread_first_list
           
 
-class Binary_search_tree:
-  def __init__(self,value=None):
-    self.node = TNode(value)
-  def add(self,value):
-    if (self.node.value == None):
-      self.node.value = value
-    else:
-      if value == self.node.value: 
-        return 'no duplicates aloowed in binary search self'
-      if (value < self.node.value):
-        if(self.node.left):
-          self.node.left.add(value)
-        else: 
-          self.node.left = Binary_search_tree(value)
-      else:
-        if(self.node.right):
-          self.node.right.add(value)
-        else: 
-          self.node.right = Binary_search_tree(value)
+# class Binary_search_tree:
+#   def __init__(self,value=None):
+#     self.node = TNode(value)
+#   def add(self,value):
+#     if (self.node.value == None):
+#       self.node.value = value
+#     else:
+#       if value == self.node.value: 
+#         return 'no duplicates aloowed in binary search self'
+#       if (value < self.node.value):
+#         if(self.node.left):
+#           self.node.left.add(value)
+#         else: 
+#           self.node.left = Binary_search_tree(value)
+#       else:
+#         if(self.node.right):
+#           self.node.right.add(value)
+#         else: 
+#           self.node.right = Binary_search_tree(value)
 
-  def in_order(self, lst = []):
-    if (self.node.left):
-        self.node.left.in_order(lst)
-    lst.append(self.node.value)
-    if (self.node.right):
-        self.node.right.in_order(lst)
-    return lst
+#   def in_order(self, lst = []):
+#     if (self.node.left):
+#         self.node.left.in_order(lst)
+#     lst.append(self.node.value)
+#     if (self.node.right):
+#         self.node.right.in_order(lst)
+#     return lst
 
-  def contains(self,value,parent= None):
-    if value == self.node.value: return True
-    if (value < self.node.value):
-      if (self.node.left):
-        return self.node.left.contains(value, self.node)
-      else: 
-        return False
-    else:
-      if (self.node.right):
-        return self.node.right.contains(value, self.node)
-      else:
-        return False
+#   def contains(self,value,parent= None):
+#     if value == self.node.value: return True
+#     if (value < self.node.value):
+#       if (self.node.left):
+#         return self.node.left.contains(value, self.node)
+#       else: 
+#         return False
+#     else:
+#       if (self.node.right):
+#         return self.node.right.contains(value, self.node)
+#       else:
+#         return False
 
-  def sum_odd(self):
-    sum = 0
-    def add_item(root):
-      if root.value % 2 ==1:
-        sum = sum+ root.values
-      if root.left:
-        add_item(root.left)
-      if root.right:
-        add_item(root.right)
-    return sum
-
-  def sum_odd(self):
-    sum = 0
-    def add_item(root):
-      nonlocal sum
-      if root.value % 2 ==1:
-        sum = sum+ root.value
-      if root.left:
-        add_item(root.left)
-      if root.right:
-        add_item(root.right)
-    add_item(self.node)    
-    return sum
+  # def sum_odd(self):
+  #   sum = 0
+  #   def add_item(root):
+  #     nonlocal sum
+  #     if root.value % 2 ==1:
+  #       sum = sum+ root.values
+  #     if root.left:
+  #       add_item(root.left)
+  #     if root.right:
+  #       add_item(root.right)
+  #   return sum
+  # @pysnooper.snoop()
+  # def sum_odd(self):
+  #  sum = 0
+  #  @pysnooper.snoop()
+  #  def add_item(root):
+  #     nonlocal sum
+      
+  #     if root.left:
+  #       return add_item(root.left)
+  #     if root.right:
+  #       return add_item(root.right)
+  #     if root.value % 2 ==1:
+  #       sum = sum+ root.value
+  #  add_item(self.node)    
+  #  return sum
+  # def sum_odd(self) :
+  
+  #   if (self.root != None): 
+  #       self.sum_odd(self.root.left) 
+          
+  #       # if node is even then print it 
+  #       if (root.key % 2 != 0):
+  #           print(root.key, end = " ") 
+  #       root.sum_odd(root.right)
+     
       
 
 
@@ -463,93 +476,93 @@ print("the maximum is " , binary_tree.find_maximum_value())
 
 
 
-# # class BSTNode(BinaryTree):
-# #     def __init__(self, value= None):
-# #         self.value = value
-# #         self.left = None
-# #         self.right = None
-# # # @pysnooper.snoop()
-# # class BinarySearchTree:
-# #     def __init__(self, root=None):
-# #         self.root = root
+# class BSTNode(BinaryTree):
+#     def __init__(self, value= None):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+# # @pysnooper.snoop()
+# class BinarySearchTree:
+#     def __init__(self, root=None):
+#         self.root = root
 
             
-# #     def pre_order(self):
-# #         pre_order_list = []
-# #         """ Pre-order traversal of our tree """
-# #         def walk(root):
-# #             if root is  None:
-# #                 raise EmptyTreeException("the tree is empty") 
-# #                 pre_order_list.append(root.value)
+#     def pre_order(self):
+#         pre_order_list = []
+#         """ Pre-order traversal of our tree """
+#         def walk(root):
+#             if root is  None:
+#                 raise EmptyTreeException("the tree is empty") 
+#                 pre_order_list.append(root.value)
 
-# #             if root.left:
-# #                 walk(root.left)
+#             if root.left:
+#                 walk(root.left)
             
-# #             if root.right:
-# #                 walk(root.right)
+#             if root.right:
+#                 walk(root.right)
                 
-# #         walk(self.root)
-# #         return ' '.join(f"{pre_order_list}")    
+#         walk(self.root)
+#         return ' '.join(f"{pre_order_list}")    
 
     
 
  
-# #     def add(self ,root, value):
-# #         if root is None:
-# #             return BSTNode(value)
-# #         else:
-# #             if root.value == value:
-# #                 return root
-# #             elif root.value < value:
-# #                 root.right = self.add(root.right, value)
-# #             else:
-# #                 root.left = self.add(root.left, value)
-# #         print (f"{value} has been added successfully")
-# #         return root
+#     def add(self ,root, value):
+#         if root is None:
+#             return BSTNode(value)
+#         else:
+#             if root.value == value:
+#                 return root
+#             elif root.value < value:
+#                 root.right = self.add(root.right, value)
+#             else:
+#                 root.left = self.add(root.left, value)
+#         print (f"{value} has been added successfully")
+#         return root
     
-# #     def pre_order(self,root):
-# #         pre_order_list = []
+#     def pre_order(self,root):
+#         pre_order_list = []
 
-# #         if root is  None:
-# #             raise EmptyTreeException("the tree is empty")
-# #         new_root = root
-# #         def pre_walk(root):
-# #             if root:
-# #                 pre_order_list.append(root.value)
-# #                 if root.left:
-# #                      pre_walk(root.left)
+#         if root is  None:
+#             raise EmptyTreeException("the tree is empty")
+#         new_root = root
+#         def pre_walk(root):
+#             if root:
+#                 pre_order_list.append(root.value)
+#                 if root.left:
+#                      pre_walk(root.left)
       
-# #                 if root.right:
-# #                      pre_walk(root.right)
+#                 if root.right:
+#                      pre_walk(root.right)
                 
-# #         pre_walk(new_root)
-# #         return ' '.join(f"{pre_order_list}")
+#         pre_walk(new_root)
+#         return ' '.join(f"{pre_order_list}")
 
    
-# #     def contains(self,root,value):
-# #         self.count_n = 0
-# #         self.found = None
-# #         if root is None:
-# #             raise EmptyTreeException("the tree is empty") 
-# #         def contains_search(root,value):
+#     def contains(self,root,value):
+#         self.count_n = 0
+#         self.found = None
+#         if root is None:
+#             raise EmptyTreeException("the tree is empty") 
+#         def contains_search(root,value):
      
-# #                 if root is None or root.value == value:
-# #                     self.found = root
-# #                     return root
+#                 if root is None or root.value == value:
+#                     self.found = root
+#                     return root
             
-# #                 if root.value < value:
-# #                     self.count_n +=1
-# #                     return contains_search(root.right,value)
-# #                 else:
-# #                      self.count_n +=1 
-# #                      return contains_search(root.left,value)
-# #         contains_search(root,value)
-# #         if self.found is None:
-# #             print("not found")
-# #             return False
+#                 if root.value < value:
+#                     self.count_n +=1
+#                     return contains_search(root.right,value)
+#                 else:
+#                      self.count_n +=1 
+#                      return contains_search(root.left,value)
+#         contains_search(root,value)
+#         if self.found is None:
+#             print("not found")
+#             return False
 
-# #         print( f"{self.found.value} has been found and the depth of it is {self.count_n}")
-# #         return True
+#         print( f"{self.found.value} has been found and the depth of it is {self.count_n}")
+#         return True
 
 
 # class Binary_search_tree:
